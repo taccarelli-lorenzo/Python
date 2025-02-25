9f0a5490862bf1ba9933ed72739defffe7d0a391
Lorenzo Taccarelli <lorenzo.taccarelli@isisbem.it>
2025-02-25 12:55:37 +0100

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+gui = tk.Tk()
+gui.title("Pack Example")
+
+button0 = tk.Button(gui, text="0", width=10, height=2)
+button1 = tk.Button(gui, text="1", width=10, height=2)
+button2 = tk.Button(gui, text="2", width=10, height=2)
+button3 = tk.Button(gui, text="3", width=10, height=2)
+button4 = tk.Button(gui, text="4", width=10, height=2)
+button5 = tk.Button(gui, text="5", width=10, height=2)
+button6 = tk.Button(gui, text="6", width=10, height=2)
+button7 = tk.Button(gui, text="7", width=10, height=2)
+button8 = tk.Button(gui, text="8", width=10, height=2)
+button9 = tk.Button(gui, text="9", width=10, height=2)
+
+buttonA = tk.Button(gui, text="A", width=10, height=2)
+buttonB = tk.Button(gui, text="B", width=10, height=2)
+buttonC = tk.Button(gui, text="C", width=10, height=2)
+
+buttonConfirm = tk.Button(gui, text="C", width=10, height=2)
+buttonDelete = tk.Button(gui, text="D", width=10, height=2)
+
+
+button1.grid(row=0, column=0)
+button2.grid(row=0, column=1)
+button3.grid(row=0, column=2)
+button4.grid(row=1, column=0)
+button5.grid(row=1, column=1)
+button6.grid(row=1, column=2)
+button7.grid(row=2, column=0)
+button8.grid(row=2, column=1)
+button9.grid(row=2, column=2)
+buttonA.grid(row=3, column=0)
+buttonB.grid(row=3, column=1)
+buttonC.grid(row=3, column=2)
+buttonConfirm.grid(row=4, column=0)
+button0.grid(row=4, column=1)
+buttonDelete.grid(row=4, column=2)
+
+gui.mainloop()
